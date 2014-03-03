bc43abfecbd0ea3f8b314750baf24c82d16f1431
herilaza <h.rakotomanana@isodom.com>
2014-03-03 17:48:45 +0400

    added fix for heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Fix change for Heroku, due to database access
+    config.assets.initialize_on_precompile = false
   end
 end
